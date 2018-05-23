17dd759b7a8230b5da7f3331c8cdd9449fa9961f
mateusmenezes95 <mateusmenezes95@gmail.com>
2018-05-23 01:02:04 -0300

    Add ROS interface

--- rqt_gui_aeropendulum/src/rqt_gui_aeropendulum/aeropendulum_gui.py
+++ rqt_gui_aeropendulum/src/rqt_gui_aeropendulum/aeropendulum_gui.py
@@ -2,7 +2,8 @@ import rospy
 from rqt_gui_py.plugin import Plugin
 from .aeropendulum_gui_widget import AeropendulumWidget
 
-# from .data_plot import DataPlot
+from aeropendulum_common_messages.srv import *
+from aeropendulum_common_messages.msg import *
 
 class Aeropendulum(Plugin):
 
@@ -18,10 +19,67 @@ class Aeropendulum(Plugin):
 
         context.add_widget(self._widget)
 
-    def testeFunc(self):
-        print "teste"
-        self._widget.pub.publish("Saporra!")
+        rospy.Subscriber("graph_plot", GraphPlotData, self.plot)
 
+        self.x = []
+        self.yAngle = []
+        self.ySetPointAngle = []
+        self.count = 0
+        self.period = 0.01
+        self.plotStep = 50
+
+        self._widget.setPointButton.clicked.connect(self.setPointRequest)
+        self.setPointClient = rospy.ServiceProxy('set_setPoint', SetPoint)
+
+        writeCsvFile = False
+        self._widget.stepResponseButton.clicked.connect(self.stepResponseRequest)
+        self.stepResponseClient = rospy.ServiceProxy('unit_step_response', SetPoint)
+
+    def plot(self, dataPlot):
+        xData = dataPlot.nSample * self.period
+        self.x.append(xData) 
+        self.yAngle.append(dataPlot.angle)
+        self.ySetPointAngle.append(dataPlot.setPointAngle)
+
+        csvData = [xData, dataPlot.angle]
+        self.writer.writerow(csvData)
+
+        # discards the old graph
+        self.ax.clear()
+
+        if self.count <= self.plotStep:
+            self.ax.set_xlim([0, self.plotStep * self.period])
+        else:
+            xMin = (self.count - (self.plotStep/2)) * self.period
+            xMax = (self.count + self.plotStep) * self.period
+            self.ax.set_xlim([xMin, xMax])
+        
+        yMin = self.ySetPointAngle[self.count] - 2
+        yMax = self.ySetPointAngle[self.count] + 0.5
+        self.ax.set_ylim([yMin, yMax])
+
+        # plot data
+        self.ax.plot(self.x, self.yAngle, 'r', label='Angulo atual')
+        self.ax.plot(self.x, self.ySetPointAngle, 'b', label='SetPoint')
+        self.ax.legend()
+
+        # refresh canvas
+        self.canvas.draw()
+
+        self.count += 1
+
+    def setPointRequest(self):
+        rospy.loginfo("Service client ok!")
+        setPointValue = float(self._widget.setPointInput.text())
+        rospy.loginfo("Sending setPoint %f", setPointValue)
+        try:
+            response = self.setPointClient(setPointValue)
+            if response.done == True:
+                rospy.loginfo("Response ok! SetPoint: %f", float(self.setPointInput.text()))
+            else:
+                rospy.loginfo("Response wrong")
+        except rospy.ServiceException, e:
+            rospy.loginfo("Service call failed: %s" %e)
     #def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure
         # This will enable a setting button (gear icon) in each dock widget title bar
